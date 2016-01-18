0f96e73025bbc03663fce21744678bc301f9adbb
Alex Forbes <alforbes@gumtree.com>
2016-01-18 12:01:24 +0000

    Ensure config dir is present before orlo::config

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,10 +3,15 @@ class orlo::install(
 ){
 
   package { ${package_name}:
-    ensure => present,
-    before => Inifile['/etc/orlo/orlo.ini'],
+    ensure  => present,
+    before  => Inifile['/etc/orlo/orlo.ini'],
+    require => File['/etc/orlo'],
   }
 
+  file { '/etc/orlo':
+    ensure => directory,
+    before => Class['orlo::config'],
+  }
 }
 
 # vim:sw=2:ts=2:et:

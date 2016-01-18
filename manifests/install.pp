41588477817248949fe26c38b166396cf49fccbb
Alex Forbes <alforbes@gumtree.com>
2016-01-18 11:57:58 +0000

    Add debian package name

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,12 @@
-class orlo::install {
+class orlo::install(
+  $package_name = 'orlo',
+){
 
-  package { 'orlo':
+  package { ${package_name}:
     ensure => present,
     before => Inifile['/etc/orlo/orlo.ini'],
   }
 
 }
 
- # vim:sw=2:ts=2:et:
+# vim:sw=2:ts=2:et:
